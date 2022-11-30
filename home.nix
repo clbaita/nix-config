d1c3da4a0957535e7913dcb6e8ec3969856ad2f2
clbaita <clbaita@outlook.com>
2022-11-30 17:53:35 +1100

    sry for theme hopping

--- home.nix
+++ home.nix
@@ -75,7 +75,7 @@ in {
 
     kitty = {
       enable = true;
-      # theme = "Ayu Mirage";
+      theme = "Ayu Mirage";
       font = {
         package = (pkgs.nerdfonts.override { fonts = [ "Iosevka" ]; });
         name = "Iosevka";
@@ -84,28 +84,6 @@ in {
         wayland_titlebar_color = "background";
         linux_display_server = "wayland";
         confirm_os_window_close = 0;
-        # Manual input of amarena theme here https://github.com/elenapan/dotfiles/blob/master/.xfiles/amarena
-        # Hopefully this is added to kitty-themes one day
-        background = "#1A2026";
-        foreground = "#FFFFFF";
-        cursor = "#FFFFFF";
-        color0 = "#242D35";
-        color1 = "#FB6396";
-        color2 = "#94CF95";
-        color3 = "#F692B2";
-        color4 = "#6EC1D6";
-        color5 = "#CD84C8";
-        color6 = "#7FE4D2";
-        color7 = "#FFFFFF";
-        color8 = "#526170";
-        color9 = "#F92D72";
-        color10 = "#6CCB6E";
-        color11 = "#F26190";
-        color12 = "#4CB9D6";
-        color13 = "#C269BC";
-        color14 = "#58D6BF";
-        color15 = "#F4F5F2";
-        #########################################################
       };
     };
   };
